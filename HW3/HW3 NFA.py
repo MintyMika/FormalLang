322769420f762c9e25a86f756814497e63848ca2
Stephen C <Stephencornelius4@gmail.com>
2023-02-20 12:10:04 -0600

    Fix: Clean code still doesn't work properly

--- HW3/HW3 NFA.py
+++ HW3/HW3 NFA.py
@@ -16,17 +16,6 @@ delta2 = [  #This is the delta for the NFA in the book for Chapter 6 Question 7
     [1<<2, 1<<2]
 ]
 
-deltaHw3 = [
-    [1<<1|1<<4,     1<<2|1<<5],         #0
-    [1<<1,      1<<0|1<<1|1<<2|1<<4],   #1
-    [1<<3|1<<4,     1<<1|1<<2|1<<5],    #2
-    [1<<0|1<<1|1<<2|1<<4,       1<<3],  #3
-    [1<<4,      1<<5],                  #4
-    [1<<0|1<<1|1<<4|1<<6,       1<<7],  #5
-    [1<<8|1<<4|1<<1, 1<<2|1<<4|1<<5],   #6
-    [1<<5,      1<<0|1<<1|1<<3|1<<6],   #7
-    [1<<7, 1<<8]                        #8
-]
 
 #NOTE: Test delta2 first because I know what the results should be
 
@@ -91,7 +80,47 @@ strings0 = [
     0b0110, 0b0101, 0b0100,
     0b0011, 0b0010, 0b0001, 0b0000
 ]
+
+#Uncomment the following code to test the parallelBitmapSearchNFA function
+# i = 1
+# for x in strings0:
+#     print(i, parallelBitmapSearchNFA(delta2, x, acceptingStates))
+#     i += 1
+
+
+#---------------------------------------------------------------
+#below is a test of the assigned homework problem
+
+#Declare the transition function
+deltaHw3 = [
+    [1<<1|1<<4,     1<<2|1<<5],         #0
+    [1<<1,      1<<0|1<<1|1<<2|1<<4],   #1
+    [1<<3|1<<4,     1<<1|1<<2|1<<5],    #2
+    [1<<0|1<<1|1<<2|1<<4,       1<<3],  #3
+    [1<<4,      1<<5],                  #4
+    [1<<0|1<<1|1<<4|1<<6,       1<<7],  #5
+    [1<<8|1<<4|1<<1, 1<<2|1<<4|1<<5],   #6
+    [1<<5,      1<<0|1<<1|1<<3|1<<6],   #7
+    [1<<7, 1<<8]                        #8
+]
+
+#Declare the accepting states
+acceptingStates = [2, 6]
+
+
+#Declare some binary numbers to test
+myNums = [
+    0b100000000, 0b100000001, 0b100000010, 0b100000011, 0b100000100, 0b100000101, 0b100000110, 0b100000111,
+    0b100001000, 0b100001001, 0b100001010, 0b100001011, 0b100001100, 0b100001101, 0b100001110, 0b100001111,
+    0b100010000, 0b100010001, 0b100010010, 0b100010011, 0b100010100, 0b100010101, 0b100010110, 0b100010111,
+    0b100011000, 0b100011001, 0b100011010, 0b100011011, 0b100011100, 0b100011101, 0b100011110, 0b100011111,
+    0b100100000, 0b100100001, 0b100100010, 0b100100011, 0b100100100, 0b100100101, 0b100100110, 0b100100111,
+    0b100101000, 0b100101001, 0b100101010, 0b100101011, 0b100101100, 0b100101101, 0b100101110, 0b100101111
+]
+
+#Loop through the numbers and test them
+
 i = 1
-for x in strings0:
-    print(i, parallelBitmapSearchNFA(delta2, x, acceptingStates))
+for x in myNums:
+    print(i, parallelBitmapSearchNFA(deltaHw3, x, acceptingStates))
     i += 1
